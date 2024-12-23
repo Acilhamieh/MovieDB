3a4781ac6917466430e06a6f66c9e03bcacc5d30
Acil <acilhamieh4@gmail.com>
2024-12-23 23:19:31 +0200

    step 3

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+//exporting the express library
 const express = require('express');
-
+//create
 const app = express();
 const PORT = 3000;
 
 // Define a basic route that responds to all URLs
+//req=>request and res=>response
 app.use((req, res) => {
     res.send('ok');
 });
@@ -11,4 +13,14 @@ app.use((req, res) => {
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+});
+//route for test
+app.get('/test', (req, res) => {
+    res.json({ status: 200, message: "ok" });
+});
+//route for time
+const moment = require('moment'); // For formatting the time
+app.get('/time', (req, res) => {
+    const currentTime = moment().format('HH:mm');
+    res.json({ status: 200, message: currentTime });
 });
