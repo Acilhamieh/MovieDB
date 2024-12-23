13f98a94d4a8e31d7167db461c0830b00fc0fb02
Acil <acilhamieh4@gmail.com>
2024-12-23 23:32:01 +0200

    step 4

--- index.js
+++ index.js
@@ -24,3 +24,18 @@ app.get('/time', (req, res) => {
     const currentTime = moment().format('HH:mm');
     res.json({ status: 200, message: currentTime });
 });
+//route for the dynamic id 
+app.get('/hello/:id?', (req, res) => {
+    const id = req.params.id || 'World';  // Default to 'World' if no ID is provided
+    res.json({ status: 200, message: `Hello, ${id}` });
+});
+// Route for /search?s=<SEARCH>
+app.get('/search', (req, res) => {
+    const search = req.query.s;
+
+    if (search) {
+        res.status(200).json({ status: 200, message: "ok", data: search });
+    } else {
+        res.status(500).json({ status: 500, error: true, message: "you have to provide a search" });
+    }
+});
